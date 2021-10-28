8971d6939d0567e6320de8beaac2b9c223f6ad85
SasakiBeryl <450988664@qq.com>
2021-10-28 15:16:44 +1100

    Initial commit

---
+++ app/src/main/java/elec5620/sydney/edu/au/smarthealth/MainActivity.java
@@ -0,0 +1,14 @@
+package elec5620.sydney.edu.au.smarthealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
