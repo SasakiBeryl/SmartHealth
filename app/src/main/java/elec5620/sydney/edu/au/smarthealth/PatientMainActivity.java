0ab413954ca0ed88f9c47c5420c9b24490dc6252
fangpeich <35284309+fangpeich@users.noreply.github.com>
2021-11-09 15:08:10 +1100

    delete recommend button on patient main page

--- app/src/main/java/elec5620/sydney/edu/au/smarthealth/PatientMainActivity.java
+++ app/src/main/java/elec5620/sydney/edu/au/smarthealth/PatientMainActivity.java
@@ -33,7 +33,6 @@ public class PatientMainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
     EditText textDescrEditText;
-    Button getRecommendButton;
     Button buttonChatBot;
     String situation = "";
     String gender="";
@@ -63,11 +62,9 @@ public class PatientMainActivity extends AppCompatActivity {
         age = getIntent().getStringExtra("age");
 
         textDescrEditText = findViewById(R.id.edittext_patient_signup_password);
-        getRecommendButton = findViewById(R.id.button_patient_login);
         buttonChatBot=findViewById(R.id.button_chatbot);
         buttonChatBot.setOnClickListener(this::onChatbotClick);
         String jsonString = " ";
-        getRecommendButton.setOnClickListener(this::onGetRecommendClick);
 
         Log.i("fangpei", "111");
 
@@ -84,64 +81,64 @@ public class PatientMainActivity extends AppCompatActivity {
     }
 
 
-    public void onGetRecommendClick(View view)
-    {
-        situation = textDescrEditText.getText().toString();
-        if (!situation.equals(""))
-        {
-            JSONObject obj= new JSONObject();
-            try {
-                obj.put("text",situation);
-                //obj.put("text","I cannot hear anything");
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            Tools.getNLPSymptoms(obj, this, new VolleyCallback()
-            {
-                @Override
-                public void onSuccess(String result) {
-                    JSONObject json = new JSONObject();
-                    try {
-                        json = new JSONObject(result);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    try {
-                        JSONArray jsonArray = json.getJSONArray("mentions");
-
-                        //HashMap<String, String> symptom = new HashMap<>();
-
-                        for (int i=0; i< jsonArray.length();i++)
-                        {
-                            Symptom symptom = new Symptom();
-                            String id = jsonArray.getJSONObject(i).getString("id");
-                            String orth = jsonArray.getJSONObject(i).getString("orth");
-                            String choice_id = jsonArray.getJSONObject(i).getString("choice_id");
-                            String name = jsonArray.getJSONObject(i).getString("name");
-                            String common_name = jsonArray.getJSONObject(i).getString("common_name");
-                            String type = jsonArray.getJSONObject(i).getString("type");
-                            symptom.setId(id);
-                            symptom.setOrth(orth);
-                            symptom.setChoid_id(choice_id);
-                            symptom.setName(name);
-                            symptom.setCommon_name(common_name);
-                            symptom.setType(type);
-                            symptoms.add(symptom);
-                        }
-                        Intent intent = new Intent(PatientMainActivity.this, ViewDoctorRecommendActivity.class);
-                        intent.putExtra("symptoms", result);
-                        viewRecommendLaucher.launch(intent);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-
-
-            });
-        }
-
-    }
+//    public void onGetRecommendClick(View view)
+//    {
+//        situation = textDescrEditText.getText().toString();
+//        if (!situation.equals(""))
+//        {
+//            JSONObject obj= new JSONObject();
+//            try {
+//                obj.put("text",situation);
+//                //obj.put("text","I cannot hear anything");
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//            Tools.getNLPSymptoms(obj, this, new VolleyCallback()
+//            {
+//                @Override
+//                public void onSuccess(String result) {
+//                    JSONObject json = new JSONObject();
+//                    try {
+//                        json = new JSONObject(result);
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+//                    try {
+//                        JSONArray jsonArray = json.getJSONArray("mentions");
+//
+//                        //HashMap<String, String> symptom = new HashMap<>();
+//
+//                        for (int i=0; i< jsonArray.length();i++)
+//                        {
+//                            Symptom symptom = new Symptom();
+//                            String id = jsonArray.getJSONObject(i).getString("id");
+//                            String orth = jsonArray.getJSONObject(i).getString("orth");
+//                            String choice_id = jsonArray.getJSONObject(i).getString("choice_id");
+//                            String name = jsonArray.getJSONObject(i).getString("name");
+//                            String common_name = jsonArray.getJSONObject(i).getString("common_name");
+//                            String type = jsonArray.getJSONObject(i).getString("type");
+//                            symptom.setId(id);
+//                            symptom.setOrth(orth);
+//                            symptom.setChoid_id(choice_id);
+//                            symptom.setName(name);
+//                            symptom.setCommon_name(common_name);
+//                            symptom.setType(type);
+//                            symptoms.add(symptom);
+//                        }
+//                        Intent intent = new Intent(PatientMainActivity.this, ViewDoctorRecommendActivity.class);
+//                        intent.putExtra("symptoms", result);
+//                        viewRecommendLaucher.launch(intent);
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//
+//
+//
+//            });
+//        }
+//
+//    }
 
 
 }
