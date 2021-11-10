3af5768920486278a7c0a05d87c8b4ba62ad4b7f
fangpeich <35284309+fangpeich@users.noreply.github.com>
2021-11-10 14:05:39 +1100

    update reminder

--- app/src/main/java/elec5620/sydney/edu/au/smarthealth/Tools.java
+++ app/src/main/java/elec5620/sydney/edu/au/smarthealth/Tools.java
@@ -2,7 +2,10 @@ package elec5620.sydney.edu.au.smarthealth;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
+
+import androidx.annotation.RequiresApi;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
@@ -13,13 +16,19 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -186,6 +195,54 @@ public class Tools {
             }
 
         }
+
+    }
+
+    /*
+A statci method to tranform received Task object to String
+ */
+    public static String taskToString (Task task)
+    {
+        Gson gson = new Gson();
+        String serializeTask = gson.toJson(task);
+        return  serializeTask;
+    }
+    /*
+    A static method transforms received String to Task object
+     */
+    public static Task stringToTask (String serializeTask)
+    {
+        Task convetedTask = new Gson().fromJson(serializeTask, Task.class);
+        return convetedTask;
+    }
+    /*
+    Calculate time difference
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String timeDiffCalculator(Date d1, Date d2)
+    {
+        Instant dateOneInstant = d1.toInstant();
+        ZonedDateTime zoneTimeOne = dateOneInstant.atZone(ZoneId.systemDefault());
+
+        Instant dateTwoInstant = d2.toInstant();
+        ZonedDateTime zoneTimeTwo = dateTwoInstant.atZone(ZoneId.systemDefault());
+
+
+        long duration = 0;
+        duration= Duration.between(zoneTimeTwo, zoneTimeOne).toMinutes();
+        /*
+        The below if statement responsible for the case that
+        the user input a date which is before the current date
+         */
+        if (!d1.after(d2))
+        {
+            duration = 0-duration;
+            String remaintime = "Overdue";
+            return  remaintime;
+        }
+
+        String remainTime = duration/(24*60)+"days "+duration/60%24+"hours "+duration%60+"minutes";
+        return remainTime;
 
     }
 }
